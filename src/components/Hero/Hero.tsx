c260d4506ad5ef596109f1766d5773edaf80a2fe
Harkor421 <66706751+Harkor421@users.noreply.github.com>
2026-05-11 21:36:15 -0500

    s

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -75,7 +75,7 @@ export function Hero({
         }}
       >
         <motion.div variants={fade}>
-          <Logo size={144} />
+          <Logo size={288} />
         </motion.div>
         <motion.h1 className={styles.title} variants={fade}>
           Rapp<span className={styles.titleAccent}>ify</span>
